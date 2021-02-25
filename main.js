681077e0595dc0def6ff0dbf757c13856a0c6e12
tuanpv29 <71309210+tuanpv29@users.noreply.github.com>
2021-02-25 21:40:10 +0700

    Create Drag/Drop app

---
+++ main.js
@@ -0,0 +1,14 @@
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  console.log(data);
+  ev.target.appendChild(document.getElementById(data));
+}
